c46485ec2326e0f5d37133e3e1dfe520824a4268
kravich2 <kravich2@uwo.ca>
2021-11-19 22:25:52 -0500

    Testing

---
+++ index.js
@@ -0,0 +1,20 @@
+const getTimes = require('./getTimes');
+const express = require('express');
+
+const app = express();
+
+// serve static contents
+app.use(express.static('static'))
+// dynamic handling
+app.get('/admin', (req, res) => {
+    res.redirect("admin.html");
+});
+app.get('/guest', (req, res) => {
+    res.redirect("guest.html");
+});
+app.get('/previous', (req, res) => {
+    // attach all previous submissions
+    let content = getTimes();
+    res.send(content);
+});
+app.listen(80);
---
+++ initDB.js
@@ -0,0 +1,35 @@
+const mysql = require('mysql')
+
+let conn = mysql.createConnection({
+    host: '34.67.72.229',
+    user: 'root',
+    password: 'password',
+    database: 'KeerthensDB',
+    port: 80,
+});
+
+conn.connect();
+
+conn.query(
+    'CREATE TABLE AdminTimes (AvailableTime varchar(10))',
+    (err, rows, fields) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('AdminTimes Created')
+        }
+    }
+)
+
+conn.query(
+    'CREATE TABLE GuestAvailabilities (GuestName varchar(30), AvailableTime varchar(10))',
+    (err, rows, fields) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('GuestAvailabilities Created')
+        }
+    }
+)
+
+conn.end();
